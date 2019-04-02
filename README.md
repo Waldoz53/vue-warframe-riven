3d14193f1fb2c287433f1b5c6d32bc0fd4bfa73c
Waldoz53 <35583388+Waldoz53@users.noreply.github.com>
2019-04-02 17:35:05 -0600

    added ascending/descending sorting

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # warframe-riven
 
-Built with Vue.js CDN, Semantic UI and Axios to get HTTP requests
+Built with Vue.js CDN (my fav JS framework), Semantic UI (my fav CSS framework) and Axios (an easy to use HTTP request library)
+All are using their CDNs
 
-A tool to find current and average prices (in platinum) for Riven Mods in Warframe, using the new public JSON files.
-The API is updated automatically weekly.
+A tool to find current and average prices (in platinum) for Riven Mods in Warframe, using the new publicly available JSON files.
+The JSON files are updated automatically weekly.
+
+Average price, standard deviation, minimum price, maximum price, and popularity are sortable
 
 To-Do:
 1. Add the basic listing of all listed items - DONE!
 2. Clean up UI/UX - DONE!
 3. Organize the listing of all listed items - DONE!
-4. Add search functionality - DONE!
+4. Add search + sort functionality - DONE!
 5. Add listings for the remaining console versions as well (Xbox One, PS4, Switch) - DONE!
-6. Add responsiveness for mobile platforms
--- main.js
+++ main.js
@@ -4,7 +4,12 @@ var app = new Vue({
     platform: "PC",
     search: '',
     info: [],
-    url: "https://n9e5v4d8.ssl.hwcdn.net/repos/weeklyRivensPC.json"
+    url: "https://n9e5v4d8.ssl.hwcdn.net/repos/weeklyRivensPC.json",
+    sortPop: true,
+    sortMax: true,
+    sortMin: true,
+    sortStdDev: true,
+    sortAvg: true
   },
   methods: {
     loadPC: function() {
@@ -30,6 +35,56 @@ var app = new Vue({
       this.info = []
       this.url = "https://n9e5v4d8.ssl.hwcdn.net/repos/weeklyRivensSWI.json"
       axios.get(this.url).then(response => (this.info = response.data))
+    },
+    sortByPop: function() {
+      if (this.sortPop == true) {
+        this.info.sort((a, b) => (a.pop > b.pop) ? 1 : -1)
+        this.info.reverse()
+        this.sortPop = false
+      } else {
+        this.info.sort((a, b) => (a.pop > b.pop) ? 1 : -1)
+        this.sortPop = true
+      }
+    },
+    sortByMax: function() {
+      if (this.sortMax == true) {
+        this.info.sort((a, b) => (a.max > b.max) ? 1 : -1)
+        this.info.reverse()
+        this.sortMax = false
+      } else {
+        this.info.sort((a, b) => (a.max > b.max) ? 1 : -1)
+        this.sortMax = true
+      }
+    },
+    sortByMin: function() {
+      if (this.sortMin == true) {
+        this.info.sort((a, b) => (a.min > b.min) ? 1 : -1)
+        this.info.reverse()
+        this.sortMin = false
+      } else {
+        this.info.sort((a, b) => (a.min > b.min) ? 1 : -1)
+        this.sortMin = true
+      }
+    },
+    sortByStdDev: function() {
+      if (this.sortStdDev == true) {
+        this.info.sort((a, b) => (a.stddev > b.stddev) ? 1 : -1)
+        this.info.reverse()
+        this.sortStdDev = false
+      } else {
+        this.info.sort((a, b) => (a.stddev > b.stddev) ? 1 : -1)
+        this.sortStdDev = true
+      }
+    },
+    sortByAvg: function() {
+      if (this.sortAvg == true) {
+        this.info.sort((a, b) => (a.avg > b.avg) ? 1 : -1)
+        this.info.reverse()
+        this.sortAvg = false
+      } else {
+        this.info.sort((a, b) => (a.avg > b.avg) ? 1 : -1)
+        this.sortAvg = true
+      }
     }
   },
   computed: {
